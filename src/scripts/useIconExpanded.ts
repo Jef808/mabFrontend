e14716195e78ab14d747c5aa1afc8c0d29678631
Jean-Francois Arbour <jf.arbour@gmail.com>
2023-01-14 14:06:32 -0500

    Cleaned directory tree

--- src/scripts/useIconExpanded.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Utility function to easily toggle between the downwards and upwards
- * caret icons.
- */
-export default function(expanded: boolean) {
-    return expanded ? "mdi-chevron-up" : "mdi-chevron-down";
-}
